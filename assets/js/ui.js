3b9c6ba2f71c73aab8c4e3e452832b52f379bb78
sachulza <sachulza@gmail.com>
2025-02-07 10:24:29 +0900

    swiper 2차 적용 완 / play버튼 미삽 / overflow 미적용

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -17,43 +17,92 @@ function MySliderMainVisual() {
     },
 
     autoHeight: true,
+    observer: true,
+    observeParents: true,
   });
 }
 
 function MySliderBookmark() {
+  let ww = window.innerWidth;
   let swiper = new Swiper(".main-bookmark-wrap .swiper", {
     slidesPerView: 4,
-    // spaceBetween: 80,
+    spaceBetween: 10,
 
     pagination: {
       el: ".main-bookmark-wrap .swiper-pagination",
       clickable: true,
+      type: "bullets",
     },
 
     navigation: {
       nextEl: ".main-bookmark-wrap .swiper-button-next",
       prevEl: ".main-bookmark-wrap .swiper-button-prev",
     },
+  });
 
-    // autoHeight: true,
+  responsiveSwiper();
+
+  function responsiveSwiper() {
+    if (ww >= 769) {
+      swiper.destroy();
+      swiper = new Swiper(".main-bookmark-wrap .swiper", {
+        slidesPerView: 4,
+        spaceBetween: 10,
+
+        pagination: {
+          el: ".main-bookmark-wrap .swiper-pagination",
+          clickable: true,
+          type: "bullets",
+        },
+
+        navigation: {
+          nextEl: ".main-bookmark-wrap .swiper-button-next",
+          prevEl: ".main-bookmark-wrap .swiper-button-prev",
+        },
+      });
+    } else if (ww < 769) {
+      swiper.destroy();
+      swiper = new Swiper(".main-bookmark-wrap .swiper", {
+        slidesPerView: 2,
+        grid: {
+          rows: 3,
+          fill: "row",
+        },
+        spaceBetween: 15,
+
+        pagination: {
+          el: ".main-bookmark-wrap .swiper-pagination",
+          clickable: true,
+          type: "fraction",
+        },
+
+        navigation: {
+          nextEl: ".main-bookmark-wrap .swiper-button-next",
+          prevEl: ".main-bookmark-wrap .swiper-button-prev",
+        },
+      });
+    }
+  }
+
+  window.addEventListener("resize", function () {
+    ww = window.innerWidth;
+    responsiveSwiper();
   });
 }
 
 function MySliderContents() {
   let swiper = new Swiper(".main-combi-contents-wrap .swiper", {
-    slidesPerView: "auto",
     slidesPerView: 1,
     spaceBetween: 60,
 
     breakpoints: {
       768: {
         slidesPerView: 1, //브라우저가 768보다 클 때
-        spaceBetween: 60,
       },
 
       1024: {
         slidesPerView: 2, //브라우저가 1024보다 클 때
-        spaceBetween: 50,
+        spaceBetween: 20,
       },
     },
 
@@ -67,10 +116,44 @@ function MySliderContents() {
       prevEl: ".main-combi-contents-wrap .swiper-button-prev",
     },
 
-    // autoHeight: true,
+    observer: true,
+    observeParents: true,
+  });
+}
+
+function MySliderComplaint() {
+  let swiper = new Swiper(".main-complaint-wrap .swiper", {
+    slidesPerView: 1,
+
+    pagination: {
+      el: ".main-complaint-wrap .swiper-pagination",
+      clickable: true,
+      type: "fraction",
+    },
+
+    navigation: {
+      nextEl: ".main-complaint-wrap .swiper-button-next",
+      prevEl: ".main-complaint-wrap .swiper-button-prev",
+    },
+
+    breakpoints: {
+      768: {
+        slidesPerView: 2, //브라우저가 768보다 클 때
+        spaceBetween: 20,
+      },
+
+      1024: {
+        slidesPerView: 3, //브라우저가 1024보다 클 때
+        spaceBetween: 20,
+      },
+    },
+
+    observer: true,
+    observeParents: true,
   });
 }
 
 MySliderMainVisual();
 MySliderBookmark();
 MySliderContents();
+MySliderComplaint();
